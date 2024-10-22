fcd5902c89d7b734377ccd469a544393d69cbae7
Ian Torres <iantorres@outlook.com>
2024-10-22 17:54:58 -0300

    Improvements added.

--- lib/sources/result.cpp
+++ lib/sources/result.cpp
@@ -1,15 +1,18 @@
 #include <expressions/result.hpp>
 
 namespace expressions {
-    result::result(const bool matches, std::unordered_map<std::string, std::string> bindings)
-    : _matches(matches), _bindings(std::move(bindings)) {
-    }
-
     bool result::matches() const {
         return _matches;
     }
 
     std::unordered_map<std::string, std::string> result::bindings() const {
         return _bindings;
+    }
+
+    std::string
+    result::get(const std::string &name) const {
+        if (_bindings.contains(name))
+            return _bindings.at(name);
+        throw std::runtime_error("The provided argument doesn't exists");
     }
 }
